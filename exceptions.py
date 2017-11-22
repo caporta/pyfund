fffdd0d98b6179c6bdabcbf1a85ee3c9fe8add2d
caporta <caporta@users.noreply.github.com>
2017-11-21 21:50:13 -0500

    Exception handling examples

---
+++ exceptions.py
@@ -0,0 +1,17 @@
+"""A module for demonstrating exceptions."""
+
+import sys
+from math import log
+
+
+def convert(s):
+    try:
+        return int(s)
+    except (ValueError, TypeError) as e:
+        print('Conversion error: {}'.format(str(e)), file=sys.stderr)
+        raise
+
+
+def string_log(s):
+    v = convert(s)
+    return log(v)
