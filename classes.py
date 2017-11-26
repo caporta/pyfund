1a275819f25e5fbe2c99700dc5c4824f1bbc75d5
caporta <caporta@users.noreply.github.com>
2017-11-26 00:05:33 -0500

    Model airtravel classes with requisite behavior

---
+++ classes.py
@@ -0,0 +1,100 @@
+class Flight:
+    def __init__(self, number, aircraft):
+        """
+        class invariants:
+            truths about an object that endure for its lifetime
+        """
+        if not number[:2].isalpha():
+            raise ValueError("No airline code in '{}'".format(number))
+
+        if not number[:2].isupper():
+            raise ValueError("Invalid airline code '{}'".format(number))
+
+        if not (number[2:].isdigit() and int(number[2:]) <= 9999):
+            raise ValueError("invalid route number '{}'".format(number))
+
+        self._number = number
+        self._aircraft = aircraft
+
+        rows, seats = self._aircraft.seating_plan()
+        self._seating = [None] + [{letter: None for letter in seats} for _ in rows]
+
+    def number(self):
+        return self._number
+
+    def airline(self):
+        return self._number[:2]
+
+    def aircraft_model(self):
+        return self._aircraft.model()
+
+    def _parse_seat(self, seat):
+        row_numbers, seat_letters = self._aircraft.seating_plan()
+
+        letter = seat[-1]
+        if letter not in seat_letters:
+            raise ValueError('Invalid seat letter {}'.format(letter))
+
+        row_text = seat[:-1]
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError('Invalid seat row {}'.format(row_text))
+
+        if row not in row_numbers:
+            raise ValueError('Invalid row number {}'.format(row))
+
+        return row, letter
+
+    def allocate_seat(self, seat, passenger):
+        row, letter = self._parse_seat(seat)
+
+        if self._seating[row][letter] is not None:
+            raise ValueError('Seat {} already occupied'.format(seat))
+
+        self._seating[row][letter] = passenger
+
+    def relocate_passenger(self, from_seat, to_seat):
+        from_row, from_letter = self._parse_seat(from_seat)
+        if self._seating[from_row][from_letter] is None:
+            raise ValueError('No passenger to relocate in seat {}'.format(from_seat))
+
+        to_row, to_letter = self._parse_seat(to_seat)
+        if self._seating[to_row][to_letter] is not None:
+            raise ValueError('Seat {} already occupied'.format(to_seat))
+
+        self._seating[to_row][to_letter] = self._seating[from_row][from_letter]
+        self._seating[from_row][from_letter] = None
+
+    def num_available_seats(self):
+        return sum(sum(1 for s in row.values() if s is None)
+                   for row in self._seating
+                   if row is not None)
+
+
+class Aircraft:
+    def __init__(self, registration, model, rows, seats_per_row):
+        self._registration = registration
+        self._model = model
+        self._rows = rows
+        self._seats_per_row = seats_per_row
+
+    def registration(self):
+        return self._registration
+
+    def model(self):
+        return self._model
+
+    def seating_plan(self):
+        return (range(1, self._rows + 1),
+                'ABCDEFGHJK'[:self._seats_per_row])
+
+
+def make_flight():
+    f = Flight('BA758', Aircraft('G-EUPT', 'Airbus A319', rows=22, seats_per_row=6))
+    f.allocate_seat('12A', 'Guido van Rossum')
+    f.allocate_seat('15F', 'Bjarne Stroustrup')
+    f.allocate_seat('15E', 'Anders Hejlsberg')
+    f.allocate_seat('1C', 'John McCarthy')
+    f.allocate_seat('1D', 'Richard Hickey')
+    return f
